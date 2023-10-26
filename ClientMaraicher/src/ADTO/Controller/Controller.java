c23a6453786c1b070e6c31157e5afeda5770a298
Tony Van de Wiele <vandewiele.tony@gmail.com>
2023-10-26 15:04:25 +0200

    :tada: Correction bug bouton supprimer + indentation

--- ClientMaraicher/src/ADTO/Controller/Controller.java
+++ ClientMaraicher/src/ADTO/Controller/Controller.java
@@ -10,8 +10,6 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.*;
 import java.net.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.sql.*;
 
 public class Controller extends WindowAdapter implements ActionListener
@@ -28,7 +26,6 @@ public class Controller extends WindowAdapter implements ActionListener
     public DataInputStream dis;
 
     private int indiceArticle = 1;
-
     private float total = 0;
 
     public String reponse;
@@ -37,13 +34,11 @@ public class Controller extends WindowAdapter implements ActionListener
     //#endregion
 
     //#region Propriétés (Getters et Setters)
-    // Getters et Setters
 
     // Client
     public ClientModel getClient() {
         return _client;
     }
-
     public void setClient(ClientModel client) {
         _client = client;
     }
@@ -53,7 +48,6 @@ public class Controller extends WindowAdapter implements ActionListener
     public Connection getCon() {
         return _con;
     }
-
     public void setCon(Connection con) {
         _con = con;
     }
@@ -63,7 +57,6 @@ public class Controller extends WindowAdapter implements ActionListener
     public Socket getSocket() {
         return _socket;
     }
-
     public void setSocket(Socket socket) {
         _socket = socket;
     }
@@ -73,7 +66,6 @@ public class Controller extends WindowAdapter implements ActionListener
     public MainFrame getMainFrame() {
         return _mainFrame;
     }
-
     public void setMainFrame(MainFrame mainFrame) {
         _mainFrame = mainFrame;
     }
@@ -83,7 +75,6 @@ public class Controller extends WindowAdapter implements ActionListener
     public String getRequete() {
         return _requete;
     }
-
     public void setRequete(String requete) {
         _requete = requete;
     }
@@ -91,7 +82,6 @@ public class Controller extends WindowAdapter implements ActionListener
     //#endregion
 
     //#region Constructeur
-
     public Controller(ClientModel client, MainFrame window, Connection con, Socket socket)
     {
         //Ininitialisation de l'application
@@ -115,8 +105,9 @@ public class Controller extends WindowAdapter implements ActionListener
             throw new RuntimeException(e);
         }
     }
+    //#endregion
 
-    //Detection d'une action
+    //#region Detection d'une action
     @Override
     public void actionPerformed(ActionEvent e)
     {
@@ -173,20 +164,21 @@ public class Controller extends WindowAdapter implements ActionListener
             on_pushButtonSupprimerArticle_clicked();
         }
     }
+    //#endregion
 
-    //A la fermeture de l'App
+    //#regionDetection de la fenetre
     @Override
     public void windowClosing(WindowEvent e)
     {
         System.out.println("Fermeture de l'application");
-        if(getClient().getConnect() == true)
+        if(getClient().getConnect())
         {
             on_pushButtonLogout_clicked();
         }
 
         try {
             getSocket().close();
-            System.out.printf("Socket fermée\n");
+            System.out.println("Socket fermée");
             getCon().close();
             System.out.println("Connexion à la base de données fermée");
         } catch (IOException | SQLException ex) {
@@ -196,6 +188,217 @@ public class Controller extends WindowAdapter implements ActionListener
     }
     //#endregion
 
+    //#region Fonction d'appui sur les boutons
+    public void on_pushButtonLogout_clicked()
+    {
+        on_pushButtonViderLePanier_clicked();
+
+        setRequete("LOGOUT#");
+        reponse = Echange();
+        reponseSplit = reponse.split("#");
+
+        if(reponseSplit[1].equals("ok"))
+        {
+            JOptionPane.showMessageDialog(null, "Déconnexion réussie !");
+            logoutOk();
+            getClient().setConnect(false);
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null, "Déconnexion échouée : " + reponseSplit[2]);
+        }
+    }
+
+    public void on_pushButtonLogin_clicked()
+    {
+        String userTxt = getMainFrame().getUserTextField().getText();
+        String passwordTxt = getMainFrame().getPasswordTextField().getText();
+
+        if(userTxt.isEmpty())
+        {
+            if(passwordTxt.isEmpty())
+            {
+                JOptionPane.showMessageDialog(getMainFrame().getFrame(),"Veuillez entrer un nom d'utilisateur et un mot de passe");
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(getMainFrame().getFrame(),"Veuillez entrer un nom d'utilisateur");
+            }
+        }
+        else
+        {
+            if(passwordTxt.isEmpty())
+            {
+                JOptionPane.showMessageDialog(getMainFrame().getFrame(),"Veuillez entrer un mot de passe");
+            }
+            else
+            {
+                // Envoi de la requete
+                if(getClient().getIsNewClient())
+                {
+                    loginOk();
+                    getClient().setConnect(true);
+                    setRequete("LOGIN#" + userTxt + "#" + passwordTxt + "#oui");
+                    reponse = Echange();
+                    reponseSplit = reponse.split("#");
+
+                    if(reponseSplit[1].equals("ok")) {
+                        JOptionPane.showMessageDialog(null, "Création de compte réussie ! Bienvenue " + userTxt);
+                        //consult_Article(indiceArticle);
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(null, "Création de compte échouée ! : " + reponseSplit[2]);
+                    }
+                }
+                else
+                {
+                    loginOk();
+                    getClient().setConnect(true);
+                    setRequete("LOGIN#" + userTxt + "#" + passwordTxt + "#non");
+                    reponse = Echange();
+
+                    reponseSplit = reponse.split("#");
+
+                    if(reponseSplit[1].equals("ok")) {
+                        JOptionPane.showMessageDialog(null, "Connexion réussie ! Bienvenue " + userTxt);
+                        consult_Article(indiceArticle);
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(null, "Connexion échouée : " + reponseSplit[2]);
+                    }
+                }
+            }
+
+        }
+    }
+
+    public void on_pushButtonAcheter_clicked()
+    {
+        String intitule;
+        String quantite;
+        float prix;
+        String prixFormater;
+        int nbrArticle;
+
+        if((int) getMainFrame().quantiteSpinner.getValue() <= 0)
+        {
+            JOptionPane.showMessageDialog(null, "Veuillez entrer une quantité supérieure à 0");
+            return;
+        }
+
+        setRequete("ACHAT#" + indiceArticle + "#" + getMainFrame().quantiteSpinner.getValue());
+
+        reponse = Echange();
+
+        reponseSplit = reponse.split("#");
+
+        if(reponseSplit[1].equals("-2"))
+        {
+            JOptionPane.showMessageDialog(null,"ACHAT REFUSE, Caddie Rempli !");
+        } else if (reponseSplit[1].equals("-1")) {
+            JOptionPane.showMessageDialog(null,"ACHAT REFUSE, Article Inexistant !");
+
+        } else if (reponseSplit[2].equals("0")) {
+            JOptionPane.showMessageDialog(null, "ACHAT REFUSE, Quantite Insufisante !");
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null,"ACHAT REUSSI !, Merci pour votre achat !");
+            quantite = reponseSplit[2];
+
+            // MAJ DU PRIX TOTAL
+            total += Integer.parseInt(quantite) * Float.parseFloat(reponseSplit[3]);
+            getMainFrame().TotalTextField.setText(String.valueOf(total));
+
+            // MAJ DE L'INTERFACE D'ACHAT
+
+            consult_Article(indiceArticle);
+
+            //MAJ DU CADDIE
+            setRequete("CADDIE#");
+            reponse = Echange();
+            reponseSplit = reponse.split("#");
+            nbrArticle = Integer.parseInt(reponseSplit[1]);
+
+            getMainFrame().model.setRowCount(0); //Vide le tableau
+
+            for(int i = 0; i < nbrArticle; i++)
+            {
+                intitule = reponseSplit[2 + (i * 3)];
+                prix = Float.parseFloat(reponseSplit[3 + (i * 3)]);
+                quantite = reponseSplit[4 + (i * 3)];
+                prixFormater = String.format("%.2f",prix);
+                ajouteArticleCaddie(intitule,prixFormater,quantite);
+            }
+        }
+    }
+
+    public void on_pushButtonConfirmerAchat_clicked()
+    {
+        getMainFrame().model.setRowCount(0);
+        setRequete("CONFIRMER");
+        Echange();
+        setTotalTextField(0);
+        total = 0;
+    }
+
+    public void on_pushButtonViderLePanier_clicked()
+    {
+        getMainFrame().model.setRowCount(0);
+        setRequete("CANCELALL");
+        Echange();
+        setTotalTextField(0);
+        total = 0;
+        consult_Article(indiceArticle);
+    }
+
+    public void on_pushButtonSupprimerArticle_clicked()
+    {
+        int indice;
+
+        if((indice = getMainFrame().model.getRowCount()) == 0)
+        {
+            JOptionPane.showMessageDialog(null,"Veuillez sélectionner un article à supprimer");
+        }
+        else
+        {
+            String intitule , reponse, prixFormater, quantite;
+            int nbrArticle;
+            float prix;
+            String[] reponseSplit;
+
+            indice--;
+            setRequete("CANCEL#" + indice);
+            reponse = Echange();
+            reponseSplit = reponse.split("#");
+            if(reponseSplit[1].equals("oui"))
+            {
+                getMainFrame().model.setRowCount(0);
+                setRequete("CADDIE#");
+                reponse = Echange();
+                reponseSplit = reponse.split("#");
+
+                nbrArticle = Integer.parseInt(reponseSplit[1]);
+                total = 0;
+
+                for(int i = 0; i < nbrArticle; i++)
+                {
+                    intitule = reponseSplit[2 + (i * 3)];
+                    prix = Float.parseFloat(reponseSplit[3 + (i * 3)]);
+                    quantite = reponseSplit[4 + (i * 3)];
+                    prixFormater = String.format("%.2f",prix);
+                    ajouteArticleCaddie(intitule,prixFormater,quantite);
+                }
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null,"Suppression échouée");
+            }
+        }
+    }
+    //#endregion
+
+    //#region Autres Fonctions utiles
     public String Echange() {
         // Envoi de la requete
         try {
@@ -326,218 +529,5 @@ public class Controller extends WindowAdapter implements ActionListener
         getMainFrame().nouveauClientCheckBox.setEnabled(false);
         getMainFrame().quantiteSpinner.setEnabled(true);
     }
-
-    public void on_pushButtonLogout_clicked()
-    {
-        on_pushButtonViderLePanier_clicked();
-
-        setRequete("LOGOUT#");
-        reponse = Echange();
-        reponseSplit = reponse.split("#");
-
-        if(reponseSplit[1].equals("ok"))
-        {
-            JOptionPane.showMessageDialog(null, "Déconnexion réussie !");
-            logoutOk();
-            getClient().setConnect(false);
-        }
-        else
-        {
-            JOptionPane.showMessageDialog(null, "Déconnexion échouée : " + reponseSplit[2]);
-        }
-    }
-
-    public void on_pushButtonLogin_clicked()
-    {
-        String userTxt = getMainFrame().getUserTextField().getText();
-        String passwordTxt = getMainFrame().getPasswordTextField().getText();
-
-        if(userTxt.isEmpty())
-        {
-            if(passwordTxt.isEmpty())
-            {
-                JOptionPane.showMessageDialog(getMainFrame().getFrame(),"Veuillez entrer un nom d'utilisateur et un mot de passe");
-            }
-            else
-            {
-                JOptionPane.showMessageDialog(getMainFrame().getFrame(),"Veuillez entrer un nom d'utilisateur");
-            }
-        }
-        else
-        {
-            if(passwordTxt.isEmpty())
-            {
-                JOptionPane.showMessageDialog(getMainFrame().getFrame(),"Veuillez entrer un mot de passe");
-            }
-            else
-            {
-                // Envoi de la requete
-                if(getClient().getIsNewClient())
-                {
-                    loginOk();
-                    getClient().setConnect(true);
-                    setRequete("LOGIN#" + userTxt + "#" + passwordTxt + "#oui");
-                    reponse = Echange();
-                    reponseSplit = reponse.split("#");
-
-                    if(reponseSplit[1].equals("ok")) {
-                        JOptionPane.showMessageDialog(null, "Création de compte réussie ! Bienvenue " + userTxt);
-                        //consult_Article(indiceArticle);
-                    }
-                    else {
-                        JOptionPane.showMessageDialog(null, "Création de compte échouée ! : " + reponseSplit[2]);
-                    }
-
-                }
-                else
-                {
-                    loginOk();
-                    getClient().setConnect(true);
-                    setRequete("LOGIN#" + userTxt + "#" + passwordTxt + "#non");
-                    reponse = Echange();
-
-                    reponseSplit = reponse.split("#");
-
-                    if(reponseSplit[1].equals("ok")) {
-                        JOptionPane.showMessageDialog(null, "Connexion réussie ! Bienvenue " + userTxt);
-                        consult_Article(indiceArticle);
-                    }
-                    else {
-                        JOptionPane.showMessageDialog(null, "Connexion échouée : " + reponseSplit[2]);
-                    }
-                }
-
-            }
-
-        }
-    }
-
-    public void on_pushButtonAcheter_clicked()
-    {
-        String intitule;
-        String quantite;
-        float prix;
-        String prixFormater;
-        int nbrArticle;
-
-        if((int) getMainFrame().quantiteSpinner.getValue() <= 0)
-        {
-            JOptionPane.showMessageDialog(null, "Veuillez entrer une quantité supérieure à 0");
-            return;
-        }
-
-        setRequete("ACHAT#" + indiceArticle + "#" + getMainFrame().quantiteSpinner.getValue());
-
-        reponse = Echange();
-
-        reponseSplit = reponse.split("#");
-
-        if(reponseSplit[1].equals("-2"))
-        {
-            JOptionPane.showMessageDialog(null,"ACHAT REFUSE, Caddie Rempli !");
-        } else if (reponseSplit[1].equals("-1")) {
-            JOptionPane.showMessageDialog(null,"ACHAT REFUSE, Article Inexistant !");
-
-        } else if (reponseSplit[2].equals("0")) {
-            JOptionPane.showMessageDialog(null, "ACHAT REFUSE, Quantite Insufisante !");
-        }
-        else
-        {
-            JOptionPane.showMessageDialog(null,"ACHAT REUSSI !, Merci pour votre achat !");
-            quantite = reponseSplit[2];
-
-            // MAJ DU PRIX TOTAL
-            total += Integer.parseInt(quantite) * Float.parseFloat(reponseSplit[3]);
-            getMainFrame().TotalTextField.setText(String.valueOf(total));
-
-            // MAJ DE L'INTERFACE D'ACHAT
-
-            consult_Article(indiceArticle);
-
-            //MAJ DU CADDIE
-            setRequete("CADDIE#");
-            reponse = Echange();
-            reponseSplit = reponse.split("#");
-            nbrArticle = Integer.parseInt(reponseSplit[1]);
-
-            getMainFrame().model.setRowCount(0); //Vide le tableau
-
-            for(int i = 0; i < nbrArticle; i++)
-            {
-                intitule = reponseSplit[2 + (i * 3)];
-                prix = Float.parseFloat(reponseSplit[3 + (i * 3)]);
-                quantite = reponseSplit[4 + (i * 3)];
-                prixFormater = String.format("%.2f",prix);
-                ajouteArticleCaddie(intitule,prixFormater,quantite);
-            }
-        }
-    }
-
-    public void on_pushButtonConfirmerAchat_clicked()
-    {
-        getMainFrame().model.setRowCount(0);
-        setRequete("CONFIRMER");
-        Echange();
-        setTotalTextField(0);
-        total = 0;
-    }
-
-    public void on_pushButtonViderLePanier_clicked()
-    {
-        getMainFrame().model.setRowCount(0);
-        setRequete("CANCELALL");
-        Echange();
-        setTotalTextField(0);
-        total = 0;
-        consult_Article(indiceArticle);
-    }
-
-    public void on_pushButtonSupprimerArticle_clicked()
-    {
-        int indice;
-
-
-
-        if((indice = getMainFrame().model.getRowCount()) == 0)
-        {
-            JOptionPane.showMessageDialog(null,"Veuillez sélectionner un article à supprimer");
-            return;
-        }
-        else{
-            String intitule , reponse, prixFormater, quantite;
-            int nbrArticle;
-            float prix;
-            String[] reponseSplit;
-            indice--;
-            setRequete("Cancel#" + indice);
-            reponse = Echange();
-            reponseSplit = reponse.split("#");
-            if(reponseSplit[1].equals("oui"))
-            {
-                getMainFrame().model.setRowCount(0);
-                setRequete("CADDIE#");
-                reponse = Echange();
-                reponseSplit = reponse.split("#");
-
-                nbrArticle = Integer.parseInt(reponseSplit[1]);
-                total = 0;
-
-                for(int i = 0; i < nbrArticle; i++)
-                {
-                    intitule = reponseSplit[2 + (i * 3)];
-                    prix = Float.parseFloat(reponseSplit[3 + (i * 3)]);
-                    quantite = reponseSplit[4 + (i * 3)];
-                    prixFormater = String.format("%.2f",prix);
-                    ajouteArticleCaddie(intitule,prixFormater,quantite);
-                }
-
-
-            }
-            else
-            {
-                JOptionPane.showMessageDialog(null,"Suppression échouée");
-            }
-        }
-    }
-
+    //#endregion
 }
